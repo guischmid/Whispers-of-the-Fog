be9b9702a55fa6751336a591cc6c74dafef290c8
Gigi <github.com.doorbell592@passmail.net>
2025-04-03 20:16:53 +0200

    add basic player movement script

---
+++ Assets/Scripts/Basic_Player_Movement.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Get input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
